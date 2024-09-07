91bbf4a6edbc61b9946a77eb015dba6cc74d2d9d
saadkhairullah <sxk7397@mavs.uta.edu>
2024-09-07 03:34:36 -0500

    Added a new about page, and added popups

---
+++ script.js
@@ -0,0 +1,9 @@
+document.getElementsByClassName("signup").addEventListner("click", function(){ 
+console.log("clicked")
+});
+// document.getElementsByClassName(".popup").addEventListener("click", function(event){
+//     const pu = document.getElementsByClassName(".popup");
+//     if (event.target == pu){
+//         pu.style.display = "none";
+//     }
+// });
