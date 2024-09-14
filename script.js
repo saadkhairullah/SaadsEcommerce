74a7a92079a5b9c0d8f08cca0645883cb158e5e6
saadkhairullah <sxk7397@mavs.uta.edu>
2024-09-13 23:28:55 -0500

    fixed X button

--- script.js
+++ script.js
@@ -1,12 +1,9 @@
 document.getElementById("signup").addEventListener("click", function(){ 
     document.querySelector(".popup").style.display = "flex";
-})
-document.getElementById("login").addEventListener("click", function(){ 
+});
+document.getElementById("signin").addEventListener("click", function(){ 
     document.querySelector(".popup").style.display = "flex";
-})
-// document.getElementsByClassName(".popup").addEventListener("click", function(event){
-//     const pu = document.getElementsByClassName(".popup");
-//     if (event.target == pu){
-//         pu.style.display = "none";
-//     }
-// });
+});
+document.getElementById("xbtn").addEventListener("click", function(){
+    document.querySelector(".popup").style.display = "none";
+});
