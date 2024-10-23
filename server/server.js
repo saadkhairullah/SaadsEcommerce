50b2be8b28f42a4eb5e11bb3e27aad94587f18fa
saadkhairullah <sxk7397@mavs.uta.edu>
2024-10-22 23:42:27 -0500

    implementing mongoose and atlas

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose')
 const homeRoutes = require('./routes/homeRoute')
 const aboutRoutes= require('./routes/aboutRoute')
 
@@ -9,7 +10,16 @@ app.use(express.json());
 app.use(homeRoutes);
 app.use(aboutRoutes);
 
-app.listen(process.env.PORT, () =>{
-console.log("server started on port", process.env.PORT);
-});
+mongoose.connect(process.env.MONGO_URL)
+// listen to requests after connection to data base
+    .then(()=>{
+        app.listen(process.env.PORT, () =>{
+            console.log("server started on port", process.env.PORT);
+            });
+    })
+    .catch((error) =>{
+        console.log(error)
+    })
 
+
+    
