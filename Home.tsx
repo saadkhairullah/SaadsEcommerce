b0e6fb6a7e685dd4f926ea9d0fa4739e3bb27901
saadkhairullah <sxk7397@mavs.uta.edu>
2025-06-14 11:38:28 -0500

    added benefits section, and made sure everything so far fits on all screen sizes comfortably

--- Home.tsx
+++ Home.tsx
@@ -61,20 +61,11 @@ const navigate = useNavigate()
         </Navbar>
         <Hero></Hero>
         <SubHero></SubHero>
-        {/* <div className='subHero'>
-        <div className='whySection'> <img src="photos/logo.webp" alt="logo" /> So Why Choose Glimmer Guard?</div>
-        <p className='subHeroHeaderTextBox'>Authentic Tint Services</p>
-        <div className='subHeroTextBox'>
-          <p>
-          At<span className='redText'> Glimmer Guard</span>, we specialize in precision-cut <span className='redText'>automotive 
-          tint</span> made to fit your vehicle perfectly—no trimming, no hassle. Based in <span className='redText'>Texas </span> and founded by an 
-          <span className='redText'> expert with hands-on tinting experience </span>, we understand what tinters really need. We offer 
-          a variety of high-quality materials, from<span className='redText'> Carbon to advanced Ceramic IR</span>, along with the essential 
-          <span className='redText'> tools</span> to get the job done right. Whether you're a <span className='redText'>pro</span> or tackling a <span className='redText'>
-            DIY </span>project, we're here to make the process smoother, faster, and better—with local support you can trust.
-            </p>
-          </div>
-        </div> */}
+        <div className='benefitsSec'>
+        <div className='benefitsHeader'>The Benefits of Window Tint</div>
+        <div className='benefitsGrid'></div>
+        
+        </div>
 
         <div className='Users'>
           {users && users.map((users) =>(
--- components/SubHero.tsx
+++ components/SubHero.tsx
@@ -5,14 +5,12 @@ return <div className='subHero'>
         <div className="SubHeroText">
         <p className='subHeroHeaderTextBox'>Authentic Tint Services</p>
         <p className='subHeroTextBox'>
-          {/* <div> */}
           At<span className='redText'> Glimmer Guard</span>, we specialize in precision-cut <span className='redText'>automotive 
           tint</span> made to fit your vehicle perfectly—no trimming, no hassle. Based in <span className='redText'>Texas </span> and founded by an 
           <span className='redText'> expert with hands-on tinting experience </span>, we understand what tinters really need. We offer 
           a variety of high-quality materials, from<span className='redText'> Carbon to advanced Ceramic IR</span>, along with the essential 
           <span className='redText'> tools</span> to get the job done right. Whether you're a <span className='redText'>pro</span> or tackling a <span className='redText'>
             DIY </span>project, we're here to make the process smoother, faster, and better—with local support you can trust.
-            {/* </div> */}
           </p>
           </div>
         </div>
